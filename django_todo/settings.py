383d51c6f6b3abf3d52aff3cfabfc68d466ae32e
MilanStefanik <stefanik.milan@gmail.com>
2020-08-14 08:16:25 +0000

    prepared to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://tbcenepthcpkdr:98f2bf05753e1a6d715ef735202f17ae0904532697cade6344d3819e99311b55@ec2-34-233-186-251.compute-1.amazonaws.com:5432/dc3kt2og5bgrs')
 }
 
 
--- todo/test_models.py
+++ todo/test_models.py
@@ -8,7 +8,7 @@ class TestModels(TestCase):
     def test_done_defaults_to_false(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
-    
+
     def test_item_string_method_return_name(self):
         item = Item.objects.create(name='Test Todo Item')
         self.assertEqual(str(item), 'Test Todo Item')
